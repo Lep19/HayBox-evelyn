025f9c3abd217049159b6aeedec02e0e1b11a79f
Lep19 <32917663+Lep19@users.noreply.github.com>
2025-12-09 20:30:26 -0800

    Update Keyboard Mapping

--- src/modes/DefaultKeyboardMode.cpp
+++ src/modes/DefaultKeyboardMode.cpp
@@ -6,26 +6,29 @@
 DefaultKeyboardMode::DefaultKeyboardMode() : KeyboardMode() {}
 
 void DefaultKeyboardMode::UpdateKeys(const InputState &inputs) {
-    Press(HID_KEY_A, inputs.lf4);
-    Press(HID_KEY_B, inputs.lf3);
-    Press(HID_KEY_C, inputs.lf2);
-    Press(HID_KEY_D, inputs.lf1);
-    Press(HID_KEY_E, inputs.lt1);
-    Press(HID_KEY_F, inputs.lt2);
-    Press(HID_KEY_G, inputs.mb3);
-    Press(HID_KEY_H, inputs.mb1);
-    Press(HID_KEY_I, inputs.mb2);
-    Press(HID_KEY_J, inputs.rf5);
-    Press(HID_KEY_K, inputs.rf6);
-    Press(HID_KEY_L, inputs.rf7);
-    Press(HID_KEY_M, inputs.rf8);
-    Press(HID_KEY_N, inputs.rf1);
-    Press(HID_KEY_O, inputs.rf2);
-    Press(HID_KEY_P, inputs.rf3);
-    Press(HID_KEY_Q, inputs.rf4);
-    Press(HID_KEY_R, inputs.rt4);
-    Press(HID_KEY_S, inputs.rt3);
-    Press(HID_KEY_T, inputs.rt5);
-    Press(HID_KEY_U, inputs.rt1);
-    Press(HID_KEY_V, inputs.rt2);
+    Press(HID_KEY_A, inputs.lf7);
+    Press(HID_KEY_B, inputs.lf6);
+    Press(HID_KEY_C, inputs.lf5);
+    Press(HID_KEY_D, inputs.lf4);
+    Press(HID_KEY_E, inputs.lf3);
+    Press(HID_KEY_F, inputs.lf2);
+    Press(HID_KEY_G, inputs.lf1);
+    Press(HID_KEY_H, inputs.lt4);
+    Press(HID_KEY_I, inputs.lt3);
+    Press(HID_KEY_J, inputs.lt1);
+    Press(HID_KEY_K, inputs.lt2);
+    Press(HID_KEY_L, inputs.mb1);
+    Press(HID_KEY_M, inputs.rt3);
+    Press(HID_KEY_N, inputs.rt4);
+    Press(HID_KEY_O, inputs.rt2);
+    Press(HID_KEY_P, inputs.rf1);
+    Press(HID_KEY_Q, inputs.rt5);
+    Press(HID_KEY_R, inputs.rf5);
+    Press(HID_KEY_S, inputs.rf6);
+    Press(HID_KEY_T, inputs.rf7);
+    Press(HID_KEY_U, inputs.rf8);
+    Press(HID_KEY_V, inputs.rf1);
+    Press(HID_KEY_W, inputs.rf2);
+    Press(HID_KEY_X, inputs.rf3);
+    Press(HID_KEY_Y, inputs.rf4);
 }
